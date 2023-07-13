6069094c45b6105da649e7c27a4353465252d86b
Abril Galardo <gallardoabril50@gmail.com>
2023-07-13 12:03:34 -0300

    Ejercicio modificado

--- ejercicios/03_instruccion_match/ejercicio_09/Match_app_09.py
+++ ejercicios/03_instruccion_match/ejercicio_09/Match_app_09.py
@@ -39,7 +39,7 @@ class App(customtkinter.CTk):
         self.combobox_estaciones = customtkinter.CTkComboBox(master=self, values=estaciones)
         self.combobox_estaciones.grid(row=1, column=0, padx=20, pady=(10, 10))
 
-        
+
         self.label_destinos = customtkinter.CTkLabel(master=self, text="Destinos")
         self.label_destinos.grid(row=2, column=0, padx=20, pady=10)
         destinos = ['Bariloche', 'Mar del plata', 'Cataratas', 'Cordoba']
@@ -50,42 +50,45 @@ class App(customtkinter.CTk):
         self.btn_informar = customtkinter.CTkButton(master=self, text="Informar", command=self.btn_informar_on_click)
         self.btn_informar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
         
-    
+
     def btn_informar_on_click(self):
-        """ estaciones = self.combobox_estaciones.get()
-        destinos = self.combobox_destino.get()
-        precio_estadia = 15000
-        
-
-        match estaciones:
-            case "Invierno":
-                if destinos == "Bariloche":
-                    aumento = 20
-                elif destinos == "Cataratas" or destinos == "Cordoba":
-                    descuento = 10
-                else:
-                    descuento = 20
-            case "Verano":
-                if destinos == "Bariloche":
-                    descuento = 20
-                elif destinos == "Cataratas" or destinos == "Cordoba":
-                    aumento = 10
-                else:
-                    aumento = 20
-            case _:
-                if destinos != "Cordoba":
-                    aumento = 10
-
-        precio_descuento = precio_estadia - (precio_estadia * descuento / 100)
-        precio_aumento = precio_estadia + (precio_estadia * aumento / 100)
-
-        alert(title="Ejercicio 9", message= f"El precio total con descuento es: {precio_descuento}")
-        alert(title="Ejercicio 9", message= f"El precio total con aumento es: {precio_aumento}") """
-            
         estaciones = self.combobox_estaciones.get()
         destinos = self.combobox_destino.get()
         precio_estadia = 15000
-        
+
+
+        match estaciones:
+            case "Invierno":
+                match destinos:
+                    case "Bariloche":
+                        porcentaje = 1.2
+                    case "Cataratas" | "Cordoba":
+                        porcentaje = 0.9
+                    case _:
+                        porcentaje = 0.8
+            case "Verano":
+                match destinos:
+                    case "Bariloche":
+                        porcentaje = 0.8
+                    case "Cataratas" | "Cordoba":
+                        porcentaje = 1.1
+                    case _:
+                        porcentaje = 1.2
+            case _:
+                match destinos:
+                    case "Cordoba":
+                        porcentaje = 1
+                    case _:
+                        porcentaje = 1.1
+
+        precio_total = precio_estadia * porcentaje
+
+        alert(title= "Ejrcicio 9", message= f"El precio total es {precio_total}")
+
+    """  estaciones = self.combobox_estaciones.get()
+        destinos = self.combobox_destino.get()
+        precio_estadia = 15000
+
 
         match estaciones:
             case "Invierno":
@@ -116,8 +119,43 @@ class App(customtkinter.CTk):
                     precio_total = precio_estadia
                     mensaje = f"El precio total es de: {precio_total}"
 
-        alert(title= "Ejercicio 9", message= mensaje)
-    
+        alert(title= "Ejercicio 9", message= mensaje) """
+
+    """ match estaciones:
+            case "Invierno":
+                match destinos:
+                    case "Bariloche":
+                        porcentaje = 20
+                        mensaje = f"El precio final es de: {precio_estadia + precio_estadia * porcentaje /100}"
+                    case "Cataratas" | "Cordoba":
+                        porcentaje = 10
+                        mensaje = f"El precio final es de: {precio_estadia - precio_estadia * porcentaje /100}"
+                    case _:
+                        porcentaje = 20
+                        mensaje = f"El precio final es de: {precio_estadia - precio_estadia * porcentaje /100}"
+            case "Verano":
+                match destinos:
+                    case "Bariloche":
+                        porcentaje = 20
+                        mensaje = f"El precio final es de: {precio_estadia - precio_estadia * porcentaje /100}"
+                    case "Cataratas" | "Cordoba":
+                        porcentaje = 10
+                        mensaje = f"El precio final es de: {precio_estadia + precio_estadia * porcentaje /100}"
+                    case _:
+                        porcentaje = 20
+                        mensaje = f"El precio final es de: {precio_estadia + precio_estadia * porcentaje /100}"
+            case _:
+                match destinos:
+                    case "Cordoba":
+                        mensaje = f"El precio final es de: {precio_estadia}"
+                    case _:
+                        porcentaje = 10
+                        mensaje = f"El precio final es de: {precio_estadia + precio_estadia * porcentaje /100}"
+
+
+        alert(title= "Ejrcicio 9", message= mensaje) """
+
+
 if __name__ == "__main__":
     app = App()
     app.geometry("300x300")
